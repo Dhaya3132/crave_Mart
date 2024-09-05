b9989bd795624c650c4b0bf305cf11db0624b763
Dhaya3132 <dhayalanu103@gmail.com>
2024-09-05 10:45:12 +0530

    feat - footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import Navbar from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Outlet />
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    </>
   )
 }
 

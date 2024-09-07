54e6227b410d1c7a8b3eb6d1061502c6aa15d8ce
Dhaya3132 <dhayalanu103@gmail.com>
2024-09-07 19:35:28 +0530

    feat - login Page

--- frontend/src/components/LoginPopup/Login.jsx
+++ frontend/src/components/LoginPopup/Login.jsx
@@ -4,27 +4,27 @@ import { assets } from '../../assets/frontend_assets/assets';
 const Login = ({ setShowLogin }) => {
   const [currentState, setCurrentState] = useState('Login')
   return (
-    <div id='login-Form' className='bg-loginColor absolute z-10 w-full'>
-      <form id="login-container">
-        <div id="login-title">
-          <h2>{currentState}</h2>
-          <img src={assets.cross_icon} onClick={() => setShowLogin(false)} />
+    <div id='login-Form' className='z-50 fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center'>
+      <form id="login-container" className='bg-white p-5 rounded-lg'>
+        <div id="login-title" className='flex justify-between items-center mb-4'>
+          <h2 className='font-semibold text-2xl'>{currentState}</h2>
+          <img src={assets.cross_icon} onClick={() => setShowLogin(false)} className='w-5 h-5' />
         </div>
-        <div id="login-Input">
-          {currentState === 'Login' ? <></> : <input type="text" placeholder='Enter your Name' required />}
-          <input type="email" placeholder='Enter your Email' required />
-          <input type="password" placeholder='Enter your Password' required />
+        <div id="login-Input" className='flex flex-col gap-4'>
+          {currentState === 'Login' ? <></> : <input type="text" placeholder='Enter your Name' required className='p-2 border rounded-md' />}
+          <input type="email" placeholder='Enter your Email' required className='p-2 border rounded-md' />
+          <input type="password" placeholder='Enter your Password' required className='p-2 border rounded-md' />
         </div>
-        <button>{currentState === 'Sign Up' ? 'Create an account' : 'Login'}</button>
-        <div id="login-Condition">
+        <button className='text-center w-full bg-orange-500 text-white p-2 mt-3 rounded-md'>{currentState === 'Sign Up' ? 'Create an account' : 'Login'}</button>
+        <div id="login-Condition" className='flex gap-1 mt-3'>
           <input type="checkbox" />
-          <p>By continuing, i agree to the terms of use & privacy policy</p>
+          <p className='font-light text-sm'>By continuing, i agree to the terms of use & privacy policy</p>
         </div>
-        {currentState === 'Login' ? <p>Create new account ? <span onClick={() => setCurrentState('Sign Up')} className='cursor-pointer'>Click here</span></p> :
-          <p>Already have an account ? <span onClick={() => setCurrentState('Login')} className='cursor-pointer' >Login</span></p>}
+        {currentState === 'Login' ? <p className='mt-3'>Create new account ? <span onClick={() => setCurrentState('Sign Up')} className='cursor-pointer text-blue-500'>Click here</span></p> :
+          <p className='mt-3'>Already have an account ? <span onClick={() => setCurrentState('Login')} className='cursor-pointer text-blue-500' >Login</span></p>}
       </form>
     </div>
   )
 }
 
-export default Login
+export default Login;

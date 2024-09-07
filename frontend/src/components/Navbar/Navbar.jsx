007fe88d6ebc550e486b7d0d299732d3149e0cb2
Dhaya3132 <dhayalanu103@gmail.com>
2024-09-07 20:55:11 +0530

    feat - cartPage

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({ setShowLogin }) => {
     <nav>
       <div className='flex justify-between items-center font-medium text-sm' id='navBar'>
         <div id="leftPart" className='flex gap-4'>
-          <img src={assets.Logo} alt="logo" className='w-32' />
+          <Link to='/'><img src={assets.Logo} alt="logo" className='w-32' /></Link>
           <ul id="navBar-Menu" className='cursor-pointer md:flex hidden gap-7 items-center'>
             <Link to='/'><li onClick={() => setMenu('home')} className={menu === 'home' ? 'underline underline-offset-4' : ''}>Home</li></Link>
             <a href='#menu' onClick={() => setMenu('menu')} className={menu === 'menu' ? 'underline underline-offset-4' : ''}>Menu</a>
@@ -20,7 +20,7 @@ const Navbar = ({ setShowLogin }) => {
         <div id='rightPart' className='md:flex items-center gap-7 hidden'>
           <img src={assets.search_icon} alt="search-icon" className='w-5' />
           <div id='cart' className='relative'>
-            <img src={assets.basket_icon} alt="cart" className='w-5' />
+            <Link to='/cart'><img src={assets.basket_icon} alt="cart" className='w-5' /></Link>
             <div id='dot' className='w-2 h-2 bg-orange-500 rounded-full absolute -top-2 -right-1'></div>
           </div>
           <button className='text-white px-5 py-2.5 bg-black rounded-full text-xs' onClick={() => setShowLogin(true)}>Sign in/ Login</button>
@@ -33,9 +33,7 @@ const Navbar = ({ setShowLogin }) => {
           <ul id="navBar-Menu" className='cursor-pointer flex flex-col gap-3 justify-start items-start text-base font-normal'>
             <li onClick={() => setMenu('home')} className={menu === 'home' ? 'underline underline-offset-8' : ''}>Home</li>
             <li onClick={() => setMenu('menu')} className={menu === 'menu' ? 'underline underline-offset-8' : ''}>Menu</li>
-            <li onClick={() => setMenu('blog')} className={menu === 'blog' ? 'underline underline-offset-8' : ''}>Blog</li>
             <li onClick={() => setMenu('contact')} className={menu === 'contact' ? 'underline underline-offset-8' : ''}>Contact us</li>
-            <li onClick={() => setMenu('mobile-app')} className={menu === 'mobile-app' ? 'underline underline-offset-8' : ''}>Mobile-app</li>
             <li onClick={() => setMenu('cart')} className={menu === 'cart' ? 'underline underline-offset-8' : ''}>Cart</li>
             <li onClick={() => setMenu('signin')} className={menu === 'signin' ? 'underline underline-offset-8' : ''}>Sign in/Loging</li>
           </ul>

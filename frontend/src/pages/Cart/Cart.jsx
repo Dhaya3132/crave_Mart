56bb18a5cadcd0729bb3a50db3cb288c8e727fb8
Dhaya3132 <dhayalanu103@gmail.com>
2024-09-07 21:23:26 +0530

    feat - cartPage

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useContext } from 'react';
+import { StoreContext } from '../../context/StoreContext';
+import { useSelector } from "react-redux";
 
 const Cart = () => {
+  const {food_list} = useContext(StoreContext);
+  const cartItems = useSelector(state => state.cart.cartItem);
   return (
     <div id='cart'>
       <div id="cart-item">
@@ -13,6 +18,22 @@ const Cart = () => {
           <p>Remove</p>
         </div>
         <hr />
+        {
+          food_list.map((item, index) => {
+            if (cartItems[item._id] > 0) {
+              return (
+                <div key={index}>
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{item.price * cartItems[item._id]}</p>
+                  <p>x</p>
+                </div>
+              )
+            }
+          })
+        }
       </div>
     </div>
   )
